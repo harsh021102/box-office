fb2e36c2dab2a2d3bdbb044c1972933be79712ff
harsh021102 <harsh021102@gmail.com>
2022-07-11 23:38:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React  from "react";
+import {Switch, Route} from 'react-router-dom'
 function App() {
   return (
-    <div>
-    </div>
+    <Switch>
+      <Route exact path="/">This is home page</Route>
+      <Route exact path="/starred">This is starred</Route>
+      <Route>this is 404 page</Route>
+    </Switch>
   );
 }
 

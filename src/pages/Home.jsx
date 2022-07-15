19ba5f9b870411f595ad66488d6b638c44d03ff4
harsh021102 <harsh021102@gmail.com>
2022-07-15 16:48:27 +0530

    Displaying API Results - Custom Render Function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,41 @@
 import React,{useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
-
+import { apiGet } from '../misc/config'
 const Home = () => {
   const [input,setInput] = useState('')
+  const [results,setResults] = useState(null)
 
   const onInputChange =(event)=>{
     setInput(event.target.value)
-    // console.log(event.target.value)
   }
+
   const onSearch = async () =>{
-    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-    const data  = await response.json()
-    // console.log(data)
+    apiGet(`/search/shows?q=${input}`).then(result=>setResults(result))
   }
+
   const onKeyDown = (event) => {
     if(event.keyCode===13)
       onSearch()
   }
+
+  const renderResults = () => {
+    if(results && results.length === 0)
+    {
+      return <div>no results</div>
+    }
+    if(results && results.length > 0)
+    {
+      return <div>{results.map((item)=> <div key={item.show.name}>{item.show.name}</div>)}</div>
+    }
+  }
+
   return (
     <MainPageLayout >
       <input type="text" onKeyDown={onKeyDown} onChange={onInputChange} value={input}/>
       <button type="button" onClick={onSearch}>Search</button>
+      {
+        renderResults()
+      }
     </MainPageLayout>
   )
 }

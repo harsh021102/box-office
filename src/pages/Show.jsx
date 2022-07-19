b71ffdd5756244448d5a6e3a62f2d3ae6f0d063c
harsh021102 <harsh021102@gmail.com>
2022-07-20 02:25:27 +0530

    Creating Show Page: Part 2

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -5,13 +5,37 @@ const Show = () => {
     const {id} = useParams()
 
     const [show,setShow] = useState(null)
+    const [isLoading,setIsLoading] = useState(true)
+    const [error,setError] = useState(null);
 
     useEffect( ()=> {
+
+        let isMounted = true;
+
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results=>{
-            setShow(results)
+                if(isMounted)
+                {
+                    setShow(results)
+                    setIsLoading(false)
+                }
+        }).catch(err=>{
+            if(isMounted)
+            {
+                setError(err.message)
+                setIsLoading(false)
+            }
         })
+
+        return ()=> {
+            isMounted = false;
+        }
     },[id])
     console.log("show:",show);
+
+    if(isLoading)
+    {
+        return <div>Data is being loaded</div>
+    }
     return (
         <div>this is show</div>
     )

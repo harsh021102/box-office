4e6c539bba4669911a668a5f4e56d8d52210bd7a
harsh021102 <harsh021102@gmail.com>
2022-07-24 22:38:19 +0530

    Creating Show Page - Part 4

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,5 +1,9 @@
 import React,{useEffect, useReducer} from 'react'
 import {useParams} from 'react-router-dom'
+import Cast from '../components/show/Cast'
+import Details from '../components/show/Details'
+import Seasons from '../components/show/Seasons'
+import ShowMainData from '../components/show/ShowMainData'
 import { apiGet } from '../misc/config'
 
 const reducer = (prevState,action) => {
@@ -52,7 +56,31 @@ const Show = () => {
         return <div>Data is being loaded</div>
     }
     return (
-        <div>this is show</div>
+        <div>
+            <ShowMainData 
+            image={show.image} 
+            name={show.name} 
+            rating={show.rating}
+            summary={show.summary}
+            tags={show.genres}/>
+            <div>
+                <h2>Details</h2>
+                <Details
+                status={show.status}
+                network={show.network}
+                premiered={show.premiered}/>
+            </div>
+            <div>
+                <h2>Seasons</h2>
+                <Seasons
+                seasons={show._embedded.seasons}/>
+            </div>
+            <div>
+                <h2>Cast</h2>
+                <Cast
+                cast={show._embedded.cast}/>
+            </div>
+        </div>
     )
 }
 

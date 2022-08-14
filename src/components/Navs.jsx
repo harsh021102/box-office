8caeff86080bfb2a96ddbc42f3646f7b346c4806
harsh021102 <harsh021102@gmail.com>
2022-08-14 21:58:55 +0530

    Styling The Entire App - Part 1

--- src/components/Navs.jsx
+++ src/components/Navs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-
+import { useLocation } from 'react-router'
+import {NavList,LinkStyled} from './Nav.styled'
+ 
 const LINKS  = [
   {to: '/', text: 'Home'},
   {to: '/starred',text: 'Starred'}
@@ -8,16 +9,20 @@ const LINKS  = [
 
 
 const Navs = () => {
+  const location = useLocation()
+
+  // console.log('location',location);
+
   return (
     <div>
-        <ul>
+        <NavList>
         {
           LINKS.map((item)=>
           <li key={item.to}>
-            <Link to={item.to}>{item.text}</Link>
+            <LinkStyled to={item.to} className={item.to===location.pathname ? 'active' : ''}>{item.text}</LinkStyled>
           </li>)
         }
-        </ul>
+        </NavList>
     </div>
   )
 }

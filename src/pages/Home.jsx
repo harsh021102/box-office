3f0699351cb307ae616797f84aee581acea6cf40
harsh021102 <harsh021102@gmail.com>
2022-08-19 00:40:08 +0530

    Revert "Styling The Entire App - Part 1"
    This reverts commit 8caeff86080bfb2a96ddbc42f3646f7b346c4806.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 import { apiGet } from '../misc/config'
 import ShowGrid from '../components/show/ShowGrid'
@@ -9,38 +9,36 @@ import CustomRadio from '../components/CustomRadio'
 
 const Home = () => {
 
-  const [input,setInput] = useLastQuery('')
-  const [results,setResults] = useState(null)
-  const [searchOption,setSearchOption] = useState('shows')
+  const [input, setInput] = useLastQuery('')
+  const [results, setResults] = useState(null)
+  const [searchOption, setSearchOption] = useState('shows')
 
   const isShowsSearch = searchOption === 'shows';
 
-  const onInputChange =(event)=>{
+  const onInputChange = (event) => {
     setInput(event.target.value)
   }
 
-  const onSearch = async () =>{
-    apiGet(`/search/${searchOption}?q=${input}`).then(result=>{
+  const onSearch = async () => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result)
     })
     console.log(results);
   }
 
   const onKeyDown = (event) => {
-    if(event.keyCode===13)
+    if (event.keyCode === 13)
       onSearch()
   }
 
   const renderResults = () => {
-    if(results && results.length === 0)
-    {
+    if (results && results.length === 0) {
       return <div>no results</div>
     }
-    if(results && results.length > 0)
-    {
-      return results[0].show 
-      ?( <ShowGrid data={results}/> )
-      :( <ActorGrid data={results}/>)   
+    if (results && results.length > 0) {
+      return results[0].show
+        ? (<ShowGrid data={results} />)
+        : (<ActorGrid data={results} />)
     }
   }
 
@@ -51,24 +49,24 @@ const Home = () => {
 
   return (
     <MainPageLayout >
-      <SearchInput type="text" onKeyDown={onKeyDown} onChange={onInputChange} value={input} placeholder="Search for something"/>
+      <SearchInput type="text" onKeyDown={onKeyDown} onChange={onInputChange} value={input} placeholder="Search for something" />
 
       <RadioInputsWrapper>
         <div>
           <CustomRadio
-          label="Shows"
-          id="shows-search" 
-          value="shows" 
-          checked={isShowsSearch} 
-          onChange={onRadioChange}
+            label="Shows"
+            id="shows-search"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
           />
         </div>
         <div>
           <CustomRadio
             label="Actors"
-            id="actors-search" 
-            value="people" 
-            checked={!isShowsSearch} 
+            id="actors-search"
+            value="people"
+            checked={!isShowsSearch}
             onChange={onRadioChange}
           />
         </div>
